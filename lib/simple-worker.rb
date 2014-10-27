f12161315f28d2074ee99813ea416755f836007b
kurochan <kuro@kurochan.org>
2014-10-27 17:02:05 +0900

    implement

--- lib/simple-worker.rb
+++ lib/simple-worker.rb
@@ -1,5 +1,22 @@
 require "simple-worker/version"
 
 module SimpleWorker
-  # Your code goes here...
+  class Worker < Queue
+    def initialize(num = 1)
+      super()
+      @workers = []
+
+      num.times do |i|
+        @workers << Thread.start do
+          loop do
+            pop.call(i)
+          end
+        end
+      end
+    end
+
+    def push(obj = nil, &block)
+      super(obj || block)
+    end
+  end
 end
